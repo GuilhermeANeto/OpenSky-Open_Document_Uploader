312d3cb2f1463b9c9aeb6f4e37776d78d1bf954c
Guilherme Augusto Neto <netoguilhermeaugusto@gmail.com>
2021-12-12 21:56:09 -0300

    feat - database created

---
+++ OpenSkyDB/dbo/Tables/Document.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Document] (
+    [IdDocument]             INT NOT NULL PRIMARY KEY IDENTITY,
+    [Title]          NVARCHAR (200)  NOT NULL,
+    [Description]    NVARCHAR (2000) NULL,
+    [Version]        NVARCHAR (30)   NULL,
+    [FileName]       NVARCHAR (255)  NOT NULL,
+    [FilePath]       NVARCHAR (260)  NOT NULL,
+    [DateTimeUpload] DATETIME        NOT NULL, 
+    [User] INT NULL
+);
---
+++ OpenSkyDB/dbo/Tables/UpdatesLog.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].UpdatesLog
+(
+	[IdUpdate] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [DocumentId] INT NOT NULL,
+    [DateTimeModification] DATETIME NOT NULL, 
+    [UploadsNewFile] BIT NULL, 
+    [LastVersion] NVARCHAR(30) NULL, 
+    [NewVersion] NVARCHAR(30) NULL, 
+    [User] INT NULL, 
+    CONSTRAINT [FK_UpdatesLog_Document] FOREIGN KEY ([IdUpdate]) REFERENCES [Document]([IdDocument]) 
+)
