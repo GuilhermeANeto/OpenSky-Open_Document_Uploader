7c28472465bd7ac7480feb6f1328bf8cde562fbb
Guilherme Augusto Neto <netoguilhermeaugusto@gmail.com>
2021-12-16 10:46:03 -0300

    feat - edicao de documento - falta a a parte da API

--- OpenSky/Scripts/Document.js
+++ OpenSky/Scripts/Document.js
@@ -11,57 +11,125 @@ function BuscarDocumentos() {
             var descricao = obj.Description.length > 100 ? obj.Description.substring(0, 100) + "..." : obj.Description;
             var versao = !obj.version ? "" : obj.version
 
-            var html = '<div class="row documentoDaLista">';
-            html += '       <div class="form-group form-group-sm col-sm-6 col-sm-offset-3">';
+            var html = '<div id="document_' + obj.IdDocument + '" class="row documentoDaLista">';
+            html += '       <div class="form-group form-group-sm col-sm-6 col-sm-offset-3" style="margin-bottom:0">';
             html += '           <a href="#" class="list-group-item">';
-            html += '           <h4 class="list-group-item-heading">' + titulo + '<small class="vercao">' + versao + '</small>'
+            html += '           <h4 class="list-group-item-heading">' + titulo + '<small class="versao">' + versao + '</small>'
             html += !obj.EditDate ? '' : '< small class="pull-right" style = "margin-top:5px" > Modificado:' + obj.EditDate + '</small >';
             html += '           </h4 >';
             html += '           <p class="list-group-item-text">' + descricao + '</p>';
             html += '           </a>';
             html += '       </div>';
-            html += '   <div style="margin-top: 25px">';
-            html += '       <button type="button" class="btn btn-sm btn-default" style="" onclick="BuscarDocumento(this);"><i class="fa fa-edit"></i></button>';
-            html += '       <button type="button" class="btn btn-sm btn-default" style="" onclick="BuscarDocumentos();"><i class="fa fa-download"></i></button>';
+            html += '       <div style="margin-top: 25px">';
+            html += '           <button type="button" class="btn btn-sm btn-default" style="" onclick="BuscarDocumento(' + obj.IdDocument + ');"><i class="fa fa-edit"></i></button>';
+            html += '           <button type="button" class="btn btn-sm btn-default" style="" onclick="BuscarDocumentos(' + obj.IdDocument + ');"><i class="fa fa-download"></i></button>';
+            html += '       </div>';
+            html += '       <span class="titulo" style="display:none">' + obj.Title + '</span>';
+            html += '       <span class="descricao" style="display:none">' + obj.Description + '</span>';
+            html += '       <span class="id" style="display:none">' + obj.IdDocument + '</span>';
+            html += '       <span class="nomeArquivo" style="display:none">' + obj.FileName + '</span>';
             html += '   </div>';
-            html += '   <span class="titulo" style="display:none">' + obj.Title + '</span>';
-            html += '   <span class="descricao" style="display:none">' + obj.Description + '</span>';
-            html += '   <span class="id" style="display:none">' + obj.Id + '</span>';
-            html += '   <span class="nomeArquivo" style="display:none">' + obj.FileName + '</span>';
-            html += '</div>';
 
             $("#divDocumentList").append(html);
         });
     });
 }
 
-function BuscarDocumento() {
+function BuscarDocumento(id) {
+    ToggleDivCadastro(true);
     $(".spnEditing").text("Editar");
-    $('#divDocInfo').slideDown();
+    $("#labelAdicionarNovoArquivo").show();
+    $('#divLabelsArquivo').css("margin-top", "30px");
+    $("#fileArquivo").hide();
+
+    var titulo = $("#document_" + id).find(".titulo").text();
+    var versao = $("#document_" + id).find(".versao").text();
+    var descricao = $("#document_" + id).find(".descricao").text();
+    var nomeArquivo = $("#document_" + id).find(".nomeArquivo").text();
+
+    $("#hddIdDocumentoEdicao").val(id);
+    $("#txtTitulo").val(titulo);
+    $("#txtDescricao").val(descricao);
+    $("#txtVersao").val(versao);
+    $("#fileArquivo").val("");
 }
 
 function SalvarDocumento() {
+    if (!ValidarCampos())
+        return
+
     var dados = {
+        id: $("#hddIdDocumentoEdicao").val(),
         titulo: $("#txtTitulo").val(),
         descricao: $("#txtDescricao").val(),
-        versao: $("#txtVersao").val()
-    }
+        versao: $("#fileArquivo").val(),
+        versao: $("#txtVersao").val(),
+        adicionarArquivoEdicao: $("#chkAdicionarNovoArquivo").prop("checked"),
+        versao: $("#fileArquivo").val()
+    };
+
+    var url = "/api/DocumentAPI";
+    $.get(url, dados, function (data) { }
+
 };
 
-function get() {
-    var url = "/api/DocumentAPI";
-    $.post(url, { value: 1 }, function (data) {
-        data.forEach(obj => console.log(obj.IdDocument))
-    });
+function ValidarCampos() {
+    if ($("#txtTitulo").val() == "") {
+        $('#spnCampoObrigatorio').text("Título");
+        $('#navbar-notification').slideDown();
+        return false;
+    } else {
+        $('#navbar-notification').slideUp();
+    }
 
+    if ($("#txtDescricao").val() == "") {
+        $('#spnCampoObrigatorio').text("Descrição");
+        $('#navbar-notification').slideDown();
+        return false;
+    } else {
+        $('#navbar-notification').slideUp();
+    }
+    if ($("#hddIdDocumentoEdicao").val() == "") {
+        if ($('#fileArquivo').get(0).files.length == 0) {
+            $('#spnCampoObrigatorio').text("O Arquivo");
+            $('#navbar-notification').slideDown();
+            return false
+        } else {
+            $('#navbar-notification').slideUp();
+        }
+    }
+    return true;
+}
+
+function ToggleDivCadastro(editando) {
+    if (editando)
+        $('#divDocInfo').slideDown();
+    else {
+        $('#divDocInfo').slideToggle();
+    }
+    LimparCampos();
+}
+
+function ToggleInputArquivo() {
+    if ($("#chkAdicionarNovoArquivo").prop("checked")) {
+        $('#fileArquivo').slideDown();
+        $('#divLabelsArquivo').css("margin-top", "0");
+    } else {
+        $('#fileArquivo').slideUp();
+        $('#divLabelsArquivo').css("margin-top", "30px");
+    }
 }
 
 function LimparCampos() {
     $(".spnEditing").text("Cadastrar Novo");
+    $("#labelAdicionarNovoArquivo").hide();
+    $("#fileArquivo").show();
+    $('#divLabelsArquivo').css("margin-top", "0");
+
+    $("#chkAdicionarNovoArquivo").prop("checked", "");
+    $("#hddIdDocumentoEdicao").val("");
     $("#txtTitulo").val("");
     $("#txtDescricao").val("");
     $("#txtVersao").val("");
-    $("#txtVersao").val("");
-    $("#txtDataUltimaModificacao").val("");
     $("#fileArquivo").val("");
 }
